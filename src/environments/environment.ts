29da6c77ca75b21b54c026f3e7f097af591c2a18
Leonel Jesus Perez Moscoso (OTI) <lperez@senamhi.gob.pe>
2021-05-20 12:04:05 -0500

    menu agregado con inventario

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: true,
-  // url_api: 'http://190.119.131.17:8080/sisbien',
+  //url_api: 'http://190.119.131.17:8080/sisbien',
   // --proxy-config proxy.conf.json
-  url_api: 'http://10.10.20.27:8080/sisbien'
+   url_api: 'http://10.10.20.27:8080/sisbien'
 };
 
 /*
